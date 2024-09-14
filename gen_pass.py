b9768591daf970eabe70e2fbb39c82ff1af0fac7
José Cabarcas <132719140+Josefct439@users.noreply.github.com>
2024-09-14 11:06:14 -0500

    Update gen_pass.py

--- gen_pass.py
+++ gen_pass.py
@@ -1,9 +1,9 @@
 import random
-elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-pass_length = int(input("Introduzca la longitud del pase: "))
 
-password = ""
-for i in range(pass_length):
-    password += random.choice(elements)
-
-print(password)
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    
+    return(password)
